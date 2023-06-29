055d2d1355b44bbaa6733d1deef172fa449a91d9
piter50 <piter50@users.noreply.github.com>
2023-06-29 11:22:37 -0400

    Add files via upload
    Uploading heavens edge sliding map code

---
+++ Maps/js/maps.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.container");
+        var pos = event.target.value;
+
+        element.find("div.biomes").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+    });
+
+});
